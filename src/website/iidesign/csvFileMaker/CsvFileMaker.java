6f031ad29eff016e5cfe7929b186699703759866
kotaroyamame <yamame@fi.chu.jp>
2016-05-28 13:20:41 -0400

    整形

--- src/website/iidesign/csvFileMaker/CsvFileMaker.java
+++ src/website/iidesign/csvFileMaker/CsvFileMaker.java
@@ -1,4 +1,5 @@
 package website.iidesign.csvFileMaker;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,169 +8,169 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
 public class CsvFileMaker {
 
-		private String filepath;
-		private String filename;
+	private String filepath;
+	private String filename;
 
-		public CsvFileMaker(String path,String filename) {
-			this.setFilepath(path);
-			this.setFilename(filename);
-		}
-		
-		
-		public void filepathSet(String filepath){
-			this.filepath=filepath;
-		}
-		
-		public String filepathGet(){
-			return this.filepath;
-		}
-		
-		public void setFilename(String filename){
-			this.filename=filename;
-		}
-		
-		public void writeFile(String value) throws IOException{
-			this.writeFile(value,true);
-		}
-		
-		public void writeFile(String value,Boolean bool) throws IOException{
-			File file = new File(filepath+"/"+this.filename+".csv");
-			FileWriter filewriter = new FileWriter(file, bool);
-			filewriter.write(value);
-			filewriter.close();
-		}
-		
-		public String readFile(){
-			String str=null;
-			StringBuffer stbf=new StringBuffer();
-			try {
-				File file = new File(filepath+"/"+this.filename+".csv");
-				if (checkBeforeReadfile(file)) {
-					FileReader filereader = new FileReader(file);
-					BufferedReader br = new BufferedReader(filereader);
-					str=br.readLine();
-					while(str!=null){	
-						stbf.append(str);
-					}
+	public CsvFileMaker(String path, String filename) {
+		this.setFilepath(path);
+		this.setFilename(filename);
+	}
 
-					filereader.close();
-					return stbf.toString();
-				} else {
-					System.out.println("File is not there");
+	public void filepathSet(String filepath) {
+		this.filepath = filepath;
+	}
+
+	public String filepathGet() {
+		return this.filepath;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public void writeFile(String value) throws IOException {
+		this.writeFile(value, true);
+	}
+
+	public void writeFile(String value, Boolean bool) throws IOException {
+		File file = new File(filepath + "/" + this.filename + ".csv");
+		FileWriter filewriter = new FileWriter(file, bool);
+		filewriter.write(value);
+		filewriter.close();
+	}
+
+	public String readFile() {
+		String str = null;
+		StringBuffer stbf = new StringBuffer();
+		try {
+			File file = new File(filepath + "/" + this.filename + ".csv");
+			if (checkBeforeReadfile(file)) {
+				FileReader filereader = new FileReader(file);
+				BufferedReader br = new BufferedReader(filereader);
+				str = br.readLine();
+				while (str != null) {
+					stbf.append(str);
 				}
-			} catch (FileNotFoundException e) {
-				System.out.println(e);
-			} catch (IOException e) {
-				System.out.println(e);
+
+				filereader.close();
+				return stbf.toString();
+			} else {
+				System.out.println("File is not there");
 			}
-			
-			return stbf.toString();
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		} catch (IOException e) {
+			System.out.println(e);
 		}
 
-		public ArrayList<String> getTextList() {
-			int i = 0;
-			// 行数チェック
-			StringBuffer buffer = new StringBuffer();
-			buffer.append(this.getFilepath());
-			buffer.append("/");
-			buffer.append(this.getFilename());
-			String fullpath = buffer.toString();
+		return stbf.toString();
+	}
 
-			int line = this.getLineLength(this.getFilename());
-			ArrayList<String> lineList = new ArrayList<String>();
-			// 行ごと配列に収納
-			try {
-				File file = new File(fullpath);
-				if (checkBeforeReadfile(file)) {
-					FileReader filereader = new FileReader(file);
-					BufferedReader br = new BufferedReader(filereader);
-					String str;
-					while (i < line) {
-						str = br.readLine();
-						lineList.add(str);		
-						i++;
-					}
-					filereader.close();
-				} else {
-					System.out.println("File is not there");
+	public ArrayList<String> getTextList() {
+		int i = 0;
+		// 行数チェック
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(this.getFilepath());
+		buffer.append("/");
+		buffer.append(this.getFilename());
+		String fullpath = buffer.toString();
+
+		int line = this.getLineLength(this.getFilename());
+		ArrayList<String> lineList = new ArrayList<String>();
+		// 行ごと配列に収納
+		try {
+			File file = new File(fullpath);
+			if (checkBeforeReadfile(file)) {
+				FileReader filereader = new FileReader(file);
+				BufferedReader br = new BufferedReader(filereader);
+				String str;
+				while (i < line) {
+					str = br.readLine();
+					lineList.add(str);
+					i++;
 				}
-			} catch (FileNotFoundException e) {
-				System.out.println(e);
-			} catch (IOException e) {
-				System.out.println(e);
+				filereader.close();
+			} else {
+				System.out.println("File is not there");
 			}
-			return lineList;
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		} catch (IOException e) {
+			System.out.println(e);
 		}
+		return lineList;
+	}
 
-		public String[] getFileList() {
-			File dir = new File(this.getFilepath());
-			return dir.list();
-		}
+	public String[] getFileList() {
+		File dir = new File(this.getFilepath());
+		return dir.list();
+	}
 
-		private boolean checkBeforeReadfile(File file) {
-			if (file.exists()) {
-				if (file.isFile() && file.canRead()) {
-					return true;
-				}
-			}
-			return false;
-		}
-
-		public int getLineLength(String filename) {
-			int lineCount = 0;
-
-			BufferedReader bra = null;
-			StringBuffer buffer = new StringBuffer();
-			buffer.append(getFilepath());
-			buffer.append("/");
-			buffer.append(filename);
-			try {
-				FileReader fr = new FileReader(buffer.toString());
-				bra = new BufferedReader(fr);
-
-				String line;
-				line = bra.readLine();
-
-				while (line != null) {
-					lineCount++;
-					line = bra.readLine();
-				}
-			} catch (FileNotFoundException e) {
-				System.out.println(e);
-
-			} catch (IOException e) {
-				System.out.println(e);
-			} finally {
-				try {
-					if (bra != null) {
-						bra.close();
-					}
-				} catch (IOException e) {
-					System.out.println(e);
-				}
-			}
-			return lineCount;
-		}
-
-		public boolean accept(String name) {
-
-			if (name.matches(".*\\.csv$")) {
+	private boolean checkBeforeReadfile(File file) {
+		if (file.exists()) {
+			if (file.isFile() && file.canRead()) {
 				return true;
 			}
+		}
+		return false;
+	}
 
-			return false;
+	public int getLineLength(String filename) {
+		int lineCount = 0;
+
+		BufferedReader bra = null;
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(getFilepath());
+		buffer.append("/");
+		buffer.append(filename);
+		try {
+			FileReader fr = new FileReader(buffer.toString());
+			bra = new BufferedReader(fr);
+
+			String line;
+			line = bra.readLine();
+
+			while (line != null) {
+				lineCount++;
+				line = bra.readLine();
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+
+		} catch (IOException e) {
+			System.out.println(e);
+		} finally {
+			try {
+				if (bra != null) {
+					bra.close();
+				}
+			} catch (IOException e) {
+				System.out.println(e);
+			}
 		}
-		public String getFilename(){
-			return this.filename;
-		}
-		public String getFilepath() {
-			return filepath;
+		return lineCount;
+	}
+
+	public boolean accept(String name) {
+
+		if (name.matches(".*\\.csv$")) {
+			return true;
 		}
 
-		public void setFilepath(String filepath) {
-			this.filepath = filepath;
-		}
+		return false;
+	}
+
+	public String getFilename() {
+		return this.filename;
+	}
+
+	public String getFilepath() {
+		return filepath;
+	}
+
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
 }
